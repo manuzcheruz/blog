c0a9aa113951ab5a4a0972e86796b7dc84b0ea19
cherucole <cheruyotcheruyot@gmail.com>
2019-10-20 11:46:42 +0300

    updated settings white

--- blog/settings.py
+++ blog/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Tinymce
 
