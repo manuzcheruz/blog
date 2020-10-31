7c6309a710c2de0687328e43350a9b57a1f05f57
manuzcheruz <kipkemoi.emanuel@gmail.com>
2020-10-31 19:20:03 +0300

    auth classes

--- blog/settings.py
+++ blog/settings.py
@@ -210,7 +210,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
